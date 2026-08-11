79117630ef8cb7fb2b6cc653ba728350efdba701
paokurtis-a11y <paokurtis@gmail.com>
2026-08-11 09:48:32 +0200

    wire station module into api

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -4,8 +4,9 @@ import { ClientsModule } from './clients/clients.module';
 import { EventsModule } from './events/events.module';
 import { PresetsModule } from './presets/presets.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { StationModule } from './station/station.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule, ClientsModule, EventsModule, PresetsModule],
+  imports: [PrismaModule, AuthModule, ClientsModule, EventsModule, PresetsModule, StationModule],
 })
 export class AppModule {}
