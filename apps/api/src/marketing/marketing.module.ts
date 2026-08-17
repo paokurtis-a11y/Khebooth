874c0d8c54dcce05e380cd295a9a7a01b3df100c
paokurtis-a11y <paokurtis@gmail.com>
2026-08-18 00:23:59 +0200

    feat(api): expose public marketing promotion service

--- apps/api/src/marketing/marketing.module.ts
+++ apps/api/src/marketing/marketing.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MarketingController } from './marketing.controller';
 import { MarketingService } from './marketing.service';
+import { PublicMarketingService } from './public-marketing.service';
 
 @Module({
   controllers:[MarketingController],
-  providers:[MarketingService],
-  exports:[MarketingService],
+  providers:[MarketingService,PublicMarketingService],
+  exports:[MarketingService,PublicMarketingService],
 })
 export class MarketingModule {}
