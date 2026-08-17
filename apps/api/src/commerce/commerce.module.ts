1c2b287cec5e8b1adbd3a35aff73c51a1474b325
paokurtis-a11y <paokurtis@gmail.com>
2026-08-18 00:17:16 +0200

    feat(api): register advanced site content service

--- apps/api/src/commerce/commerce.module.ts
+++ apps/api/src/commerce/commerce.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CommerceController } from './commerce.controller';
 import { CommerceService } from './commerce.service';
+import { SiteContentService } from './site-content.service';
 
 @Module({
-  controllers: [CommerceController],
-  providers: [CommerceService],
-  exports: [CommerceService],
+  controllers:[CommerceController],
+  providers:[CommerceService,SiteContentService],
+  exports:[CommerceService],
 })
 export class CommerceModule {}
