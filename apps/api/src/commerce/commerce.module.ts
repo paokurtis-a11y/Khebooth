1bead8308193be437928d98a7bf1348bc1b22828
paokurtis-a11y <paokurtis@gmail.com>
2026-08-18 00:22:29 +0200

    feat(api): wire subscriber access and promotional checkout

--- apps/api/src/commerce/commerce.module.ts
+++ apps/api/src/commerce/commerce.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MarketingModule } from '../marketing/marketing.module';
 import { CommerceController } from './commerce.controller';
 import { CommerceService } from './commerce.service';
+import { CustomerAccessService } from './customer-access.service';
+import { PromotionCheckoutService } from './promotion-checkout.service';
 import { SiteContentService } from './site-content.service';
 
 @Module({
+  imports:[MarketingModule],
   controllers:[CommerceController],
-  providers:[CommerceService,SiteContentService],
+  providers:[CommerceService,CustomerAccessService,PromotionCheckoutService,SiteContentService],
   exports:[CommerceService],
 })
 export class CommerceModule {}
