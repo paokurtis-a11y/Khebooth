70da99217f3c989f6be6c05a3f8b7bb036e90165
paokurtis-a11y <paokurtis@gmail.com>
2026-08-18 12:51:37 +0200

    feat(auth): enable permission guard in events module

--- apps/api/src/events/events.module.ts
+++ apps/api/src/events/events.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module';
 import { SubscriptionsModule } from '../subscriptions/subscriptions.module';
 import { EventsAutomationController } from './events-automation.controller';
 import { EventsController } from './events.controller';
 import { EventsService } from './events.service';
 
 @Module({
-  imports: [SubscriptionsModule],
+  imports: [AuthModule,SubscriptionsModule],
   controllers: [EventsController, EventsAutomationController],
   providers: [EventsService],
   exports: [EventsService],
