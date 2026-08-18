1c1981693965e35848cbb2efe5fda4b40bd2f585
paokurtis-a11y <paokurtis@gmail.com>
2026-08-18 12:55:16 +0200

    feat(auth): enable permission guard in team module

--- apps/api/src/team/team.module.ts
+++ apps/api/src/team/team.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module';
 import { TeamController } from './team.controller';
 import { TeamService } from './team.service';
 
-@Module({ controllers: [TeamController], providers: [TeamService] })
+@Module({ imports:[AuthModule],controllers: [TeamController], providers: [TeamService] })
 export class TeamModule {}
