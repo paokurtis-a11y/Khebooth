9935eb72dcb4e837fc67c6dd7026c1b28c4a0a7c
paokurtis-a11y <paokurtis@gmail.com>
2026-08-18 12:52:30 +0200

    feat(auth): enable permission guard in presets module

--- apps/api/src/presets/presets.module.ts
+++ apps/api/src/presets/presets.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module';
 import { PresetsController } from './presets.controller';
 import { PresetsService } from './presets.service';
 
 @Module({
+  imports:[AuthModule],
   controllers: [PresetsController],
   providers: [PresetsService],
 })
