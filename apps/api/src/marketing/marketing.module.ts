159ef7dc5ee175933bd20cf5fc62e5bce28e202c
paokurtis-a11y <paokurtis@gmail.com>
2026-08-18 12:53:26 +0200

    feat(auth): enable permission guard in marketing module

--- apps/api/src/marketing/marketing.module.ts
+++ apps/api/src/marketing/marketing.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module';
 import { MarketingController } from './marketing.controller';
 import { MarketingService } from './marketing.service';
 import { PublicMarketingService } from './public-marketing.service';
 
 @Module({
+  imports:[AuthModule],
   controllers:[MarketingController],
   providers:[MarketingService,PublicMarketingService],
   exports:[MarketingService,PublicMarketingService],
