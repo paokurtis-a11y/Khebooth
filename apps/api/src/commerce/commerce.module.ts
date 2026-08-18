79e8b5cc9630739543273eb166d5af9ae8c3b912
paokurtis-a11y <paokurtis@gmail.com>
2026-08-18 12:54:23 +0200

    feat(auth): enable permission guard in commerce module

--- apps/api/src/commerce/commerce.module.ts
+++ apps/api/src/commerce/commerce.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module';
 import { MarketingModule } from '../marketing/marketing.module';
 import { CommerceController } from './commerce.controller';
 import { CommerceService } from './commerce.service';
@@ -10,7 +11,7 @@ import { PromotionCheckoutService } from './promotion-checkout.service';
 import { SiteContentService } from './site-content.service';
 
 @Module({
-  imports:[MarketingModule],
+  imports:[AuthModule,MarketingModule],
   controllers:[CommerceController],
   providers:[CommerceService,CustomerAccessService,LocalizedSiteService,MarketPricingService,PaymentAnalyticsService,PromotionCheckoutService,SiteContentService],
   exports:[CommerceService,MarketPricingService],
