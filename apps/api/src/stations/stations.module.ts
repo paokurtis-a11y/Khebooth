81fb586d986af8ad93829dcb72e5d22017500d70
paokurtis-a11y <paokurtis@gmail.com>
2026-08-20 00:25:52 +0200

    feat(api): register public social flow

--- apps/api/src/stations/stations.module.ts
+++ apps/api/src/stations/stations.module.ts
@@ -9,6 +9,7 @@ import { MediaSharingService } from './media-sharing.service';
 import { MediaStorageService } from './media-storage.service';
 import { ProfileStorageService } from './profile-storage.service';
 import { PublicMediaController } from './public-media.controller';
+import { PublicSocialController } from './public-social.controller';
 import { SharingBusinessService } from './sharing-business.service';
 import { StationAuthGuard } from './station-auth.guard';
 import { StationConnectionService } from './station-connection.service';
@@ -19,7 +20,7 @@ import { StationsService } from './stations.service';
 
 @Module({
   imports: [AuthModule, EventsModule, CommerceModule, SubscriptionsModule],
-  controllers: [StationsController, PublicMediaController],
+  controllers: [StationsController, PublicMediaController, PublicSocialController],
   providers: [StationsService, StationRenewalService, StationProfileService, ClientEventWorkspaceService, DesignStorageService, ProfileStorageService, MediaStorageService, MediaSharingService, SharingBusinessService, StationConnectionService, StationAuthGuard],
 })
 export class StationsModule {}
