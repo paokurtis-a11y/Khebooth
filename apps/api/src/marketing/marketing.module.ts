58214edc8407c82ac8a5137d80644a95a4131238
paokurtis-a11y <paokurtis@gmail.com>
2026-08-20 01:21:34 +0200

    feat(marketing): register report export service

--- apps/api/src/marketing/marketing.module.ts
+++ apps/api/src/marketing/marketing.module.ts
@@ -3,11 +3,12 @@ import { AuthModule } from '../auth/auth.module';
 import { MarketingController } from './marketing.controller';
 import { MarketingService } from './marketing.service';
 import { PublicMarketingService } from './public-marketing.service';
+import { ReportExportService } from './report-export.service';
 
 @Module({
   imports:[AuthModule],
   controllers:[MarketingController],
-  providers:[MarketingService,PublicMarketingService],
-  exports:[MarketingService,PublicMarketingService],
+  providers:[MarketingService,PublicMarketingService,ReportExportService],
+  exports:[MarketingService,PublicMarketingService,ReportExportService],
 })
 export class MarketingModule {}
