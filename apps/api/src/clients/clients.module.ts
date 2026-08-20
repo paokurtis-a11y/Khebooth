9c27602b770f9d3de56d4cb6f94720ca3c237bcd
paokurtis-a11y <paokurtis@gmail.com>
2026-08-20 17:45:53 +0200

    feat(api): register Enterprise form export service

--- apps/api/src/clients/clients.module.ts
+++ apps/api/src/clients/clients.module.ts
@@ -3,13 +3,14 @@ import { AuthModule } from '../auth/auth.module';
 import { ClientEnterpriseAccessService } from './client-enterprise-access.service';
 import { ClientsController } from './clients.controller';
 import { ClientsService } from './clients.service';
+import { EnterpriseFormExportService } from './enterprise-form-export.service';
 import { EnterpriseOnboardingController } from './enterprise-onboarding.controller';
 import { EnterpriseOnboardingService } from './enterprise-onboarding.service';
 
 @Module({
   imports:[AuthModule],
   controllers:[ClientsController,EnterpriseOnboardingController],
-  providers:[ClientsService,ClientEnterpriseAccessService,EnterpriseOnboardingService],
+  providers:[ClientsService,ClientEnterpriseAccessService,EnterpriseOnboardingService,EnterpriseFormExportService],
   exports:[EnterpriseOnboardingService],
 })
 export class ClientsModule {}
