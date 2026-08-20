8cdda990cbcdd06939d41c0daacde3c73e7cdaf5
paokurtis-a11y <paokurtis@gmail.com>
2026-08-20 17:42:11 +0200

    feat(api): register Enterprise onboarding services

--- apps/api/src/clients/clients.module.ts
+++ apps/api/src/clients/clients.module.ts
@@ -3,10 +3,13 @@ import { AuthModule } from '../auth/auth.module';
 import { ClientEnterpriseAccessService } from './client-enterprise-access.service';
 import { ClientsController } from './clients.controller';
 import { ClientsService } from './clients.service';
+import { EnterpriseOnboardingController } from './enterprise-onboarding.controller';
+import { EnterpriseOnboardingService } from './enterprise-onboarding.service';
 
 @Module({
   imports:[AuthModule],
-  controllers:[ClientsController],
-  providers:[ClientsService,ClientEnterpriseAccessService],
+  controllers:[ClientsController,EnterpriseOnboardingController],
+  providers:[ClientsService,ClientEnterpriseAccessService,EnterpriseOnboardingService],
+  exports:[EnterpriseOnboardingService],
 })
 export class ClientsModule {}
