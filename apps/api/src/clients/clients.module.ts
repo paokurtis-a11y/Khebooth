79b009dd7879598303a32096f395f1ed6e4bd23f
paokurtis-a11y <paokurtis@gmail.com>
2026-08-20 02:41:19 +0200

    feat(clients): register Enterprise access manager

--- apps/api/src/clients/clients.module.ts
+++ apps/api/src/clients/clients.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
+import { ClientEnterpriseAccessService } from './client-enterprise-access.service';
 import { ClientsController } from './clients.controller';
 import { ClientsService } from './clients.service';
 
 @Module({
   imports:[AuthModule],
-  controllers: [ClientsController],
-  providers: [ClientsService],
+  controllers:[ClientsController],
+  providers:[ClientsService,ClientEnterpriseAccessService],
 })
 export class ClientsModule {}
