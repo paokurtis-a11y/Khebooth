6defdb105197ea74836a8080ad090711a9fa2e87
paokurtis-a11y <paokurtis@gmail.com>
2026-08-20 20:02:08 +0200

    feat(marketing): register automated email service

--- apps/api/src/marketing/marketing.module.ts
+++ apps/api/src/marketing/marketing.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
+import { EmailMarketingController } from './email-marketing.controller';
+import { EmailMarketingService } from './email-marketing.service';
 import { GrowthIntelligenceService } from './growth-intelligence.service';
 import { MarketingController } from './marketing.controller';
 import { MarketingService } from './marketing.service';
@@ -8,8 +10,8 @@ import { ReportExportService } from './report-export.service';
 
 @Module({
   imports:[AuthModule],
-  controllers:[MarketingController],
-  providers:[MarketingService,PublicMarketingService,ReportExportService,GrowthIntelligenceService],
-  exports:[MarketingService,PublicMarketingService,ReportExportService,GrowthIntelligenceService],
+  controllers:[MarketingController,EmailMarketingController],
+  providers:[MarketingService,PublicMarketingService,ReportExportService,GrowthIntelligenceService,EmailMarketingService],
+  exports:[MarketingService,PublicMarketingService,ReportExportService,GrowthIntelligenceService,EmailMarketingService],
 })
 export class MarketingModule {}
