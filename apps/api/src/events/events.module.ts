1b485c1d29225ced613918a8f5b748aba4c6d3ea
paokurtis-a11y <paokurtis@gmail.com>
2026-08-23 22:03:21 +0200

    feat(events): register Event Ready monitoring service

--- apps/api/src/events/events.module.ts
+++ apps/api/src/events/events.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
 import { SubscriptionsModule } from '../subscriptions/subscriptions.module';
+import { EventReadinessService } from './event-readiness.service';
 import { EventsAutomationController } from './events-automation.controller';
 import { EventsController } from './events.controller';
 import { EventsService } from './events.service';
@@ -8,7 +9,7 @@ import { EventsService } from './events.service';
 @Module({
   imports: [AuthModule,SubscriptionsModule],
   controllers: [EventsController, EventsAutomationController],
-  providers: [EventsService],
-  exports: [EventsService],
+  providers: [EventsService, EventReadinessService],
+  exports: [EventsService, EventReadinessService],
 })
 export class EventsModule {}
